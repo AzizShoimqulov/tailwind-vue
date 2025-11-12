5885ff4c86c77f15a8ee64632342f424239885f1
Aziz Shoimqulov <azizshoimqulov74@gmail.com>
2025-11-12 13:22:35 +0500

    gfgf

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,14 @@
 <script setup>
 import { ref } from "vue";
 
-const message = ref("Salom Vue!");
 const counter = ref(0);
+
+function increment() {
+  counter.value++;
+}
 </script>
 
 <template>
-  <h1>{{ message }}</h1>
-  <button class="bg-slate-600" @click="counter.value++">Click: {{ counter }}</button>
+  <p>Click: {{ counter }}</p>
+  <button @click="increment">Bos</button>
 </template>
