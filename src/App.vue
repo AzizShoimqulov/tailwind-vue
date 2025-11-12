2f1d1ea1d2f1d51b9bd7c4be4d5b583b9f381180
Aziz Shoimqulov <azizshoimqulov74@gmail.com>
2025-11-12 13:19:55 +0500

    hgf

--- src/App.vue
+++ src/App.vue
@@ -2,8 +2,10 @@
 import { ref } from "vue";
 
 const message = ref("Salom Vue!");
+const counter = ref(0);
 </script>
 
 <template>
-  <h1 class="text-center text-5xl">{{ message }}</h1>
+  <h1>{{ message }}</h1>
+  <button @click="counter.value++">Click: {{ counter }}</button>
 </template>
