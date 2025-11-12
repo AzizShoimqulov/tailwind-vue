e6c891b671f4250599fbfe3cd2f5e071f16c4d1a
Aziz Shoimqulov <azizshoimqulov74@gmail.com>
2025-11-12 12:57:10 +0500

    dsad

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,8 @@
-<template>
-  <div class="min-h-screen flex items-center justify-center bg-red-500">
-   <h1 class="text-3xl">Hello</h1>
-  </div>
-</template>
-
 <script setup>
-import { ref } from 'vue'
-const count = ref(0)
+import { ref } from "vue";
+
+const message = ref("Salom Vue!");
 </script>
+
+<template>
+  <h1>{{ messa
