47870d695a929818dc76465dcfa844dab892d7d7
Aziz Shoimqulov <azizshoimqulov74@gmail.com>
2025-11-19 13:58:08 +0500

    p

--- src/App.vue
+++ src/App.vue
@@ -70,7 +70,7 @@ const ism = ref("");
 
 
 
-<!---   ppppp>>>>>>>>>>>>> v-if, v-else-if, v-else, v-show, v-for<<<<<<<<<<<<<<<<<<
+<!---   pppppp>>>>>>>>>>>>> v-if, v-else-if, v-else, v-show, v-for<<<<<<<<<<<<<<<<<<
 -->
 <template>
   <div class="p-4 flex flex-col items-center gap-4">
