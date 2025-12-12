68e23182f8bb7a5cd6ebbe8e3900da13bc8f1827
Aziz Shoimqulov <azizshoimqulov74@gmail.com>
2025-12-12 16:15:05 +0500

    /

--- src/main.js
+++ src/main.js
@@ -2,5 +2,6 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import TOdo from './todolist.vue'
 import './assets/index.css'
+import { Icon } from "@iconify/vue";
 
-createApp(App).mount('#app')
+createApp(App).component(Icon).mount('#app')
