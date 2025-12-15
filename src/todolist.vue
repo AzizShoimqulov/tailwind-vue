ecf413251f3dc8613afa4b5985fe0be68a1a47c9
Aziz Shoimqulov <azizshoimqulov74@gmail.com>
2025-12-15 12:02:48 +0500

    f

--- src/todolist.vue
+++ src/todolist.vue
@@ -2,8 +2,18 @@
   <header class="sticky top-0 z-50 transition-all duration-300">
     <section>
       <div class="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
-        <div class="">
-          <h1>AAA</h1>
+        <div class="flex flex-col items-center justify-between gap-3 md:gap-6">
+          
+          <div class="flex justify-between items-center w-full md:w-auto">
+            <a href="/" class="text-2xl font-bold text-pink-600">ShopEase</a>
+            <button
+            class="md:hidden text-gray-700 hover:text-indigo-600"
+            aria-label="Toggle mobile menu"
+            >
+            <Icon icon="mdi:menu" class="w-6 h-6"/>
+            </button>
+
+          </div>
         </div>
       </div>
     </section>
@@ -12,5 +22,7 @@
 
 
 <script setup>
+import { Icon } from '@iconify/vue';
+
 
 </script>
