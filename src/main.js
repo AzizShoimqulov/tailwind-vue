e9631e6a693ee49e05e8fcf1b563525cdef204cc
Aziz Shoimqulov <azizshoimqulov74@gmail.com>
2025-12-15 12:06:23 +0500

    c

--- src/main.js
+++ src/main.js
@@ -1,7 +1,9 @@
 import { createApp } from 'vue'
 import App from './App.vue'
-import TOdo from './todolist.vue'
 import './assets/index.css'
-import { Icon } from "@iconify/vue";
+import { Icon } from '@iconify/vue'
 
-createApp(App).component(Icon).mount('#app')
+const app = createApp(App)
+// register Icon component globally as 'Icon'
+app.component('Icon', Icon)
+app.mount('#app')
