40de6b085d32d5d2df95be138c5c0a421adaff2c
Aziz Shoimqulov <azizshoimqulov74@gmail.com>
2025-12-15 11:47:13 +0500

    e

--- src/todolist.vue
+++ src/todolist.vue
@@ -1,7 +1,8 @@
 <template>
   <header class="sticky top-0 z-50 transition-all duration-300">
     <section>
-      <div class="">
+      <div class="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
+        
 
       </div>
     </section>
