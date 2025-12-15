27c530d36bac6ad8a6789c39c751f0352f886a39
Aziz Shoimqulov <azizshoimqulov74@gmail.com>
2025-12-15 11:48:16 +0500

    sd

--- src/todolist.vue
+++ src/todolist.vue
@@ -3,7 +3,7 @@
     <section>
       <div class="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
         <div class="">
-          HEllo
+          <h1>AAA</h1>
         </div>
       </div>
     </section>
