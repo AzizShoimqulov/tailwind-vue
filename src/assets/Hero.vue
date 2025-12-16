b7ed4df5e604a11dfc143a72d85438e4bf6f7cee
Aziz Shoimqulov <azizshoimqulov74@gmail.com>
2025-12-16 16:39:51 +0500

    df

--- src/assets/Hero.vue
+++ src/assets/Hero.vue
@@ -1,7 +1,9 @@
 <template>
     <div>
-        <section>
-            <div></div>
+        <section id="home" class="bg-amber-50 scroll-mt-20 py-16">
+            <div class="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 flex flex-col-reverse lg:flex-row items-center gap-10">
+
+            </div>
         </section>
     </div>
 </template>
