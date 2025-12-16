f878d23dc11b7494c9976216370a0ca4e709986a
Aziz Shoimqulov <azizshoimqulov74@gmail.com>
2025-12-16 16:42:52 +0500

    fds

--- src/assets/Hero.vue
+++ src/assets/Hero.vue
@@ -2,7 +2,13 @@
     <div>
         <section id="home" class="bg-amber-50 scroll-mt-20 py-16">
             <div class="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 flex flex-col-reverse lg:flex-row items-center gap-10">
-
+                <!--Left Content-->
+                <div class="text-center lg:text-left max-w-xl">
+                    <h1 class="text-4xl sm:text-5xl font-bold text-amber-950 leading-tight mb-4">
+                        Discover the Best Deals on Top Products
+                    </h1>
+                    
+                </div>
             </div>
         </section>
     </div>
