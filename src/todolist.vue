174f12f7f5c656d393aed3d2b39ff8e9837eb75e
Aziz Shoimqulov <azizshoimqulov74@gmail.com>
2025-12-16 12:48:15 +0500

    sda

--- src/todolist.vue
+++ src/todolist.vue
@@ -50,8 +50,16 @@
     <nav class="bg-pink-950" aria-label="Main navigation">
       <div class="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
         <ul class="hiddin md:flex justify-center py-3 flex-wrap gap-x-6 text-sm font-medium text-white">
-
+          <li v-for="item in navItems" :key="item.id">
+            <a :href="item.link" class="hover:text-pink-300 transition-colors"{{ item.name }}></a>
+          </li>
         </ul>
+
+        <section v-if="isMobileMenuOpen"
+        class="md:hidden mt-2 bg-white rounded-lg shadow-md p-4 space-y-3 text-[#5D4037] text-center"
+        aria-label="Mobile Navigation">
+        <a v-for="item in navItems" :href="item.link" :key="item.id">{{ item.name }}</a>
+      </section>
       </div>
     </nav>
   </header>
@@ -59,8 +67,27 @@
 
 
 <script setup>
-import { ref } from 'vue'
-
+import { ref,onMounted,onUnmounted } from 'vue'
 const isScrolled = ref(false);
 const isMobileMenuOpen = ref(false);
+const navItems=[
+  {id:1, name:'Home', link:'#home'},
+  {id:2, name:'Products', link:'#products'},
+  {id:3, name:'Categories', link:'#categories'},
+  {id:4, name:'Offers', link:'#offers'},
+  {id:5, name:'About', link:'#about'},
+  {id:6, name:'Contact', link:'#contact'},
+]
+
+consthandleScroll=()=>{
+  isScrolled.value=window.scrollY>10;
+}
+
+onMounted(()=>{
+  window.addEventListener('scroll',handleScroll);
+})
+
+onUnmounted(()=>{
+  window.removeEventListener('scroll',handleScroll);
+})
 </script>
