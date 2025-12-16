36a4be474eab8000af1c3a3d053e3cdd296b14a1
Aziz Shoimqulov <azizshoimqulov74@gmail.com>
2025-12-16 12:55:46 +0500

    1

--- src/todolist.vue
+++ src/todolist.vue
@@ -27,19 +27,19 @@
 
           <!-- Icons-->
             <aside class="flex item-center justify-end space-x-4 w-full md:w-auto">
-              <button class="relative p-2 text-gray-700 hover:text pink-600" aria-label="Wishlist">
+              <button class="relative p-2 text-gray-700 hover:text-pink-600" aria-label="Wishlist">
                 <Icon icon="mdi:heart-outline" class="w-5 h-5" />
                 <span 
                   class="absolute -top-1 -right-1 bg-pink-500 text-white text-xs rounded-full h-5 w-5
                   flex items-center justify-center">3</span>
               </button>
-                <button class="relative p-2 text-gray-700 hover:text pink-600" aria-label="Wishlist">
+                <button class="relative p-2 text-gray-700 hover:text-pink-600" aria-label="Wishlist">
                 <Icon icon="mdi:cart-outline" class="w-5 h-5" />
                 <span 
                   class="absolute -top-1 -right-1 bg-pink-500 text-white text-xs rounded-full h-5 w-5
                   flex items-center justify-center">5</span>
               </button>
-                <button class="relative p-2 text-gray-700 hover:text pink-600" aria-label="Wishlist">
+                <button class="relative p-2 text-gray-700 hover:text-pink-600" aria-label="Wishlist">
                 <Icon icon="mdi:account-outline" class="w-5 h-5" />
               </button>
             </aside>
@@ -49,9 +49,9 @@
     <!-- Navbar bar-->
     <nav class="bg-pink-950" aria-label="Main navigation">
       <div class="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
-        <ul class="hiddin md:flex justify-center py-3 flex-wrap gap-x-6 text-sm font-medium text-white">
+  <ul class="hidden md:flex justify-center py-3 flex-wrap gap-x-6 text-sm font-medium text-white">
           <li v-for="item in navItems" :key="item.id">
-            <a :href="item.link" class="hover:text-pink-300 transition-colors"{{ item.name }}></a>
+            <a :href="item.link" class="hover:text-pink-300 transition-colors">{{ item.name }}</a>
           </li>
         </ul>
 
@@ -79,8 +79,8 @@ const navItems=[
   {id:6, name:'Contact', link:'#contact'},
 ]
 
-consthandleScroll=()=>{
-  isScrolled.value=window.scrollY>10;
+const handleScroll = () => {
+  isScrolled.value = window.scrollY > 10;
 }
 
 onMounted(()=>{
