8e8aab581e1d936228fd51d29680025ec81989a2
Aziz Shoimqulov <azizshoimqulov74@gmail.com>
2025-12-18 14:14:57 +0500

    df

--- src/assets/Products.vue
+++ src/assets/Products.vue
@@ -17,7 +17,9 @@
             </nav>
         </header>
 
-        
+        <section class="relative overflow-hidden">
+            <ul class="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4"></ul>
+        </section>
 
     </main>
 </section>
