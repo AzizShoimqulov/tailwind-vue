611047067ced6ad9368763a7e10c46779557b679
Aziz Shoimqulov <azizshoimqulov74@gmail.com>
2025-12-18 10:28:45 +0500

    asa

--- src/assets/Products.vue
+++ src/assets/Products.vue
@@ -1,7 +1,11 @@
 <template>
-    <div>
-        
-    </div>
+<section class="py-12 scroll-mt-20 bg-gray-50" id="products">
+    <main class="container mx-auto px-4">
+        <header class="flex justify-between items-center mb-8">
+            <h2 class="">Feartured Products</h2>
+        </header>
+    </main>
+</section>
 </template>
 
 
