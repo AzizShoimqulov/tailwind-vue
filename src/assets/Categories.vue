8bf44636da619ca2d2034bcaff33c4eff392f957
Aziz Shoimqulov <azizshoimqulov74@gmail.com>
2025-12-24 11:28:36 +0500

    hgf

--- src/assets/Categories.vue
+++ src/assets/Categories.vue
@@ -6,10 +6,14 @@
                 <h2 class="text-3xl font-bold text-amber-950 mb-3">Explore Our Fragrance Collections</h2>
                 <p class="text-lg text-gray-800 max-w-2xl mx-auto">
                     Discover scents that match your personality
-                    33:50
                 </p>
             </div>
         </div>
+
+        <!--Categories Grid-->
+         <div class="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6>
+            <div ></div>
+         </div>
     </section>
 </template>
 
