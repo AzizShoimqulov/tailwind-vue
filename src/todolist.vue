34533c25094312f992d6385645624c92d248f707
Aziz Shoimqulov <azizshoimqulov74@gmail.com>
2025-12-24 12:34:40 +0500

    1

--- src/todolist.vue
+++ src/todolist.vue
@@ -1,26 +1,26 @@
 <template>
   <header class="sticky top-0 z-50 transition-all duration-300">
-    <section :class="['w-full', isScrolled ? 'bg-gray-100/95 backdrop-blur shadow-md py-2' : 'bg-gray-100 py-4']">
+    <section :class="['w-full', isScrolled ? 'bg-gray-100/95 backdrop-blur shadow-md py-2' : 'bg-[#F3EDDF] py-4']">
       <div class="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
         <div class="flex flex-col items-center justify-between gap-3 md:gap-6 md:flex-row">
 
           <nav class="" aria-label="Main navigation">
             <div class="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
               <ul class="hidden md:flex justify-center py-3 flex-wrap gap-x-6 text-sm font-medium text-black">
-              <li v-for="item in navItems" :key="item.id">
-               <a :href="item.link" class="hover:text-pink-800 transition-colors">{{ item.name }}</a>
-              </li>
+                <li v-for="item in navItems" :key="item.id">
+                  <a :href="item.link" class="hover:text-pink-800 transition-colors">{{ item.name }}</a>
+                </li>
               </ul>
 
-        <section v-if="isMobileMenuOpen"
-        class="md:hidden mt-2 bg-white rounded-lg shadow-md p-4 space-y-3 text-[#5D4037] text-center"
-        aria-label="Mobile Navigation">
-        <a v-for="item in navItems" :href="item.link" :key="item.id"
-        class="block hover:text-amber-600 text-sm font-medium">{{ item.name }}</a>
-      </section>
-      
-      </div>
-    </nav>
+              <section v-if="isMobileMenuOpen"
+                class="md:hidden mt-2 bg-white rounded-lg shadow-md p-4 space-y-3 text-[#5D4037] text-center"
+                aria-label="Mobile Navigation">
+                <a v-for="item in navItems" :href="item.link" :key="item.id"
+                  class="block hover:text-amber-600 text-sm font-medium">{{ item.name }}</a>
+              </section>
+
+            </div>
+          </nav>
 
           <div class="flex justify-between items-center w-full md:w-auto">
             <a href="/" class="text-2xl font-bold text-pink-600">Beyoglu Karshi</a>
@@ -29,10 +29,24 @@
               <Icon icon="mdi:menu" class="w-6 h-6" />
             </button>
           </div>
+          
+          <!-- Icons-->
+          <aside class="flex item-center justify-end space-x-4 w-full md:w-auto">
+            <button class="relative p-2 text-gray-700 hover:text-pink-600" aria-label="Wishlist">
+              <Icon icon="mdi:heart-outline" class="w-5 h-5" />
+              <span class="absolute -top-1 -right-1 bg-pink-500 text-white text-xs rounded-full h-5 w-5
+              flex items-center justify-center">3</span>
+            </button>
+            <button class="relative p-2 text-gray-700 hover:text-pink-600" aria-label="Wishlist">
+              <Icon icon="mdi:cart-outline" class="w-5 h-5" />
+              <span class="absolute -top-1 -right-1 bg-pink-500 text-white text-xs rounded-full h-5 w-5
+              flex items-center justify-center">5</span>
+            </button>
+          </aside>
           <!-- Search Bar -->
           <form class="w-full md:flex-1 max-w-sm" role="search" aria-label="Site search">
             <label class="relative w-full">
-              <input type="text" placeholder="Search..." class="w-full px-3 py-2 text-[15px] border border-gray-300 rounded-full focus:outline-none
+              <input type="text" placeholder="Search..." class="w-7/12 px-3 py-2 text-[15px] border border-gray-300 rounded-full focus:outline-none
               focus:ring-2 focus:ring-indigo-500">
 
               <button type="submit"
@@ -42,25 +56,6 @@
               </button>
             </label>
           </form>
-
-          <!-- Icons-->
-            <aside class="flex item-center justify-end space-x-4 w-full md:w-auto">
-              <button class="relative p-2 text-gray-700 hover:text-pink-600" aria-label="Wishlist">
-                <Icon icon="mdi:heart-outline" class="w-5 h-5" />
-                <span 
-                  class="absolute -top-1 -right-1 bg-pink-500 text-white text-xs rounded-full h-5 w-5
-                  flex items-center justify-center">3</span>
-              </button>
-                <button class="relative p-2 text-gray-700 hover:text-pink-600" aria-label="Wishlist">
-                <Icon icon="mdi:cart-outline" class="w-5 h-5" />
-                <span 
-                  class="absolute -top-1 -right-1 bg-pink-500 text-white text-xs rounded-full h-5 w-5
-                  flex items-center justify-center">5</span>
-              </button>
-                <button class="relative p-2 text-gray-700 hover:text-pink-600" aria-label="Wishlist">
-                <Icon icon="mdi:account-outline" class="w-5 h-5" />
-              </button>
-            </aside>
         </div>
       </div>
     </section>
@@ -69,26 +64,26 @@
 
 
 <script setup>
-import { ref,onMounted,onUnmounted } from 'vue'
+import { ref, onMounted, onUnmounted } from 'vue'
 const isScrolled = ref(false);
 const isMobileMenuOpen = ref(false);
-const navItems=[
-  {id:1, name:'Home', link:'#home'},
-  {id:2, name:'Menu', link:'#products'},
-  {id:3, name:'PAGES', link:'#categories'},
-  {id:4, name:'BLOG', link:'#offers'},
-  {id:5, name:'SHOP', link:'#about'},
+const navItems = [
+  { id: 1, name: 'Home', link: '#home' },
+  { id: 2, name: 'Menu', link: '#products' },
+  { id: 3, name: 'PAGES', link: '#categories' },
+  { id: 4, name: 'BLOG', link: '#offers' },
+  { id: 5, name: 'SHOP', link: '#about' },
 ]
 
 const handleScroll = () => {
   isScrolled.value = window.scrollY > 10;
 }
 
-onMounted(()=>{
-  window.addEventListener('scroll',handleScroll);
+onMounted(() => {
+  window.addEventListener('scroll', handleScroll);
 })
 
-onUnmounted(()=>{
-  window.removeEventListener('scroll',handleScroll);
+onUnmounted(() => {
+  window.removeEventListener('scroll', handleScroll);
 })
 </script>
