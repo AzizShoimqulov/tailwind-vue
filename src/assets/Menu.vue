af1e68e7aaa35a8dd7b437dcca739fcb30ec8ce5
Aziz Shoimqulov <azizshoimqulov74@gmail.com>
2026-01-06 10:59:44 +0500

    fds

--- src/assets/Menu.vue
+++ src/assets/Menu.vue
@@ -12,7 +12,8 @@
 
 
 <script setup>
-import Menemen from '../img/image copy.png'
+import Menemen from '../menu_img/image.png'
+import Omlet from '../menu_img/image copy.png'
 const menuItems = [
     {
         id: 1,
@@ -21,5 +22,55 @@ const menuItems = [
         price: '39.000 so`m',
         image: Menemen,
     },
+    {
+        id: 2,
+        name: 'Pishloqli Omlet',
+        select: 'Nonushta',
+        price: '39.000 so`m',
+        image: Omlet,
+    },
+    {
+        id: 3,
+        name: 'Qovurmali Yumurta',
+        select: 'Nonushta',
+        price: '51.000 so`m',
+        image: Qovurmali,
+    },
+    {
+        id: 4,
+        name: 'Sujukli Yumurta',
+        select: 'Nonushta',
+        price: '51.000 so`m',
+        image: Sujukli,
+    },
+    {
+        id: 5,
+        name: 'Qovurilgan tuxum',
+        select: 'Nonushta',
+        price: '18.000 so`m',
+        image: Qovurilgan,
+    },
+    {
+        id: 6,
+        name: 'Sigar Byurek',
+        select: 'Nonushta',
+        price: '32.000 so`m',
+        image: SigarByurek,
+    },
+    {
+        id: 7,
+        name: 'Beyugludan Nonushta Set 1',
+        select: 'Nonushta',
+        price: '98.000 so`m',
+        image: Set1,
+    },
+    {
+        id: 8,
+        name: 'Beyugludan Nonushta Set 2',
+        select: 'Nonushta',
+        price: '198.000 so`m',
+        image: Set2,
+    },
+
 ];
 </script>
