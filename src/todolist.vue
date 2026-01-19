757681778d60f21d915a4c0e13bb76c7d6fb6c55
Aziz Shoimqulov <azizshoimqulov74@gmail.com>
2026-01-19 11:56:29 +0500

    dfs

--- src/todolist.vue
+++ src/todolist.vue
@@ -194,7 +194,6 @@ onUnmounted(() => {
     </div>
   </header>
   
-  <!-- MOBILE MENU OVERLAY & SIDEBAR (qolgan qismlari o'zgarishsiz) -->
   <transition enter-active-class="transition duration-300" enter-from-class="opacity-0" enter-to-class="opacity-100" leave-active-class="transition duration-200" leave-from-class="opacity-100" leave-to-class="opacity-0">
     <div v-if="isMobileMenuOpen" @click="toggleMenu" class="fixed inset-0 bg-black/50 z-[60] lg:hidden"></div>
   </transition>
